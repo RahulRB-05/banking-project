5fd261bec0451c194d6571e99b138fc05e34d16a
RahulRB-05 <mailrahulrb@gmail.com>
2025-12-22 21:10:51 +0530

    Added my file

---
+++ SenshiProject/src/test/java/com/example/SenshiProject/SenshiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SenshiProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SenshiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
